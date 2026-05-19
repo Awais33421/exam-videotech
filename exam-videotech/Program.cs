969e118dc4349539e4e6f0f349f905d265069e38
HamZil <itsme.hamzi@gmail.com>
2026-05-19 20:14:42 +0500

    Ejercicio 2.1

--- exam-videotech/Program.cs
+++ exam-videotech/Program.cs
@@ -1,9 +1,27 @@
-﻿using exam_videotech.model;
+﻿using System.Reflection;
+using exam_videotech.model;
 
 namespace exam_videotech
 {
     class Program
     {
-       
+        static void Main(string[] args)
+        {
+
+            List<Movie> movies = new List<Movie>();
+
+            movies.Add(new Movie("Jurassic Pakr", "Lambudar", 2005, false));
+            movies.Add(new Movie("Inception", "James", 2014, true));
+            movies.Add(new Movie("Harry Potter", "JK Rolling", 2002, true));
+
+            Console.WriteLine("All Movies");
+            foreach (Movie movie in movies) { 
+            
+                Console.WriteLine(movie.ToString());
+            }
+
+
+        }
+
     }
 }
