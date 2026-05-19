956103919fbd7195571c6b1e06c2ad3e4550b548
HamZil <itsme.hamzi@gmail.com>
2026-05-19 20:22:44 +0500

    Ejercicio 2.3

--- exam-videotech/Program.cs
+++ exam-videotech/Program.cs
@@ -7,8 +7,9 @@ namespace exam_videotech
     {
         static void Main(string[] args)
         {
-
+            //Apartado 2.1 - Trabajar con la lista  
             List<Movie> movies = new List<Movie>();
+            string directorName = "Nolan";
 
             movies.Add(new Movie("Jurassic Pakr", "Lambudar", 2005, false));
             movies.Add(new Movie("Inception", "James", 2014, true));
@@ -20,7 +21,40 @@ namespace exam_videotech
                 Console.WriteLine(movie.ToString());
             }
 
+            Console.WriteLine($"{directorName} Movies: ");
 
+            foreach (Movie movie in movies)
+            {
+
+                if (movie.GetDirector().Contains(directorName))
+                {
+                    Console.WriteLine(movie.ToString());
+                }
+            }
+
+            //Apartado 2.2 - Fecha de registro  
+            Console.WriteLine(" Current Date ");
+            Console.WriteLine(DateTime.Now.ToShortDateString());
+
+            //Apartado 2.3 - Guardar en fichero  
+
+        }
+        static void SaveToFile(List<Movie> movies, string path)
+        {
+            StreamWriter write = new StreamWriter(path);
+
+            //Interstellar;Christopher Nolan;2014;True 
+
+            foreach (Movie movie in movies)
+            {
+                write.WriteLine(
+                    movie.GetTitulo + ";" +
+                    movie.GetDirector + ";" +
+                    movie.GetAnyo + ";" +
+                    movie.IsDisponible
+                    );
+            }
+            write.Close();
         }
 
     }
