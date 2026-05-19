b9f98028dae0127caf63765991a27f0cd47e3d7f
HamZil <itsme.hamzi@gmail.com>
2026-05-19 20:04:35 +0500

    Ejercicio 1.1

--- exam-videotech/Program.cs
+++ exam-videotech/Program.cs
@@ -1,2 +1,9 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using exam_videotech.model;
+
+namespace exam_videotech
+{
+    class Program
+    {
+       
+    }
+}
--- exam-videotech/model/Movie.cs
+++ exam-videotech/model/Movie.cs
@@ -1,12 +1,44 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using static System.Runtime.InteropServices.JavaScript.JSType;
 
 namespace exam_videotech.model
 {
-    internal class Movies
+    public class Movie
     {
+        private string titulo;
+        private string director;
+        private int anyo;
+        private bool disponible;
+
+        public Movie(string titulo, string director,int anyo,bool disponible)
+        {
+            this.titulo = titulo;
+            this.director = director;
+            this.anyo = anyo;
+            this.disponible = disponible;
+
+        }
+        public string GetTitulo()
+        { return titulo; }
+
+        public string GetDirector()
+        { return titulo; }
+
+        public int GetAnyo()
+        { return anyo; }
+
+        public bool IsDisponible()
+        { return disponible; }
+
+        public string ToString()
+        {
+            return titulo + "-" + director +"(" + anyo +")";
+        }
+
     }
 }
